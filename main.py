8ced6adfcb3a406dc2b82e9d0c217189ae5fc4c4
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2021-01-16 20:18:51 -0300

    style: modified comments

--- main.py
+++ main.py
@@ -2,18 +2,6 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=W0613, C0116
 # type: ignore[union-attr]
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-First, a few callback functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Example of a bot-user conversation using ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
 
 import logging
 import unidecode
@@ -34,15 +22,17 @@ from telegram.ext import (
 from dotenv import load_dotenv
 load_dotenv()
 
-# Enable logging
+# Ativa logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
+# Estados validos
 CHOOSING_NAME, INTERESTS = range(2)
 
+# Variaveis de ambiente
 TOKEN = getenv("TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 
@@ -65,6 +55,7 @@ def begin(update: Update, context: CallbackContext) -> int:
 
 def set_name(update: Update, context: CallbackContext) -> int:
     text = update.message.text
+    # Salvar o nome do usuario
     context.user_data['name'] = text
     update.message.reply_text(f'Legal {text}, do que voce gosta?')
 
@@ -83,51 +74,28 @@ def regular_choice(update: Update, context: CallbackContext) -> int:
 
     return INTERESTS
 
-# def received_information(update: Update, context: CallbackContext) -> int:
-#     user_data = context.user_data
-#     text = update.message.text
-#     category = user_data['choice']
-#     user_data[category] = text
-#     del user_data['choice']
-
-#     update.message.reply_text(
-#         "Neat! Just so you know, this is what you already told me:"
-#         f"{facts_to_str(user_data)} You can tell me more, or change your opinion"
-#         " on something.",
-#         reply_markup=markup,
-#     )
-
-#     return CHOOSING
-
-
-def done(update: Update, context: CallbackContext) -> int:
-    user_data = context.user_data
-    if 'choice' in user_data:
-        del user_data['choice']
-
-    update.message.reply_text(f"I learned these facts about you. Until next time!")
-
-    user_data.clear()
-    return ConversationHandler.END
+def fallback(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text(f"Não entendi o que voce quis dizer")
+    return INTERESTS
 
 def main() -> None:
 
+    # Ler o arquivo de conversacao
     conversation_file = open("conversation", "r")
     conversation_lines = conversation_file.read().split('\n')
 
+    # Processar os dados de conversacao
     for i in range(0, len(conversation_lines), 2):
         conversation_map[conversation_lines[i]] = conversation_lines[i+1]
 
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
+    # Persitencia dos dados do usuario
     pp = PicklePersistence(filename='savedata')
+    # Passar o token para o bot
     updater = Updater(TOKEN,persistence=pp, use_context=True)
 
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # Add conversation handler with the states
+    # Estados de conversacao
     conv_handler = ConversationHandler(
         entry_points=[
             CommandHandler('start', start),
@@ -151,24 +119,17 @@ def main() -> None:
             #         Filters.text & ~(Filters.command | Filters.regex('^Done$')), regular_choice
             #     )
             # ],
-            # TYPING_REPLY: [
-            #     MessageHandler(
-            #         Filters.text & ~(Filters.command | Filters.regex('^Done$')),
-            #         received_information,
-            #     )
-            # ],
         },
-        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)],
+        fallbacks=[MessageHandler(Filters.text, fallback)],
     )
 
     dispatcher.add_handler(conv_handler)
 
-    # Start the Bot
+    # Iniciar o Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # O bot executa até que o processo seja terminado com Ctrl-C
+    # ou receba um SIGINT, SIGTERM or SIGABRT.
     updater.idle()
 
 # TODO: chamar o usuario (ser ativo)
