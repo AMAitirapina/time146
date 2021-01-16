ed52c573e5e0b1ae0de0cf70dbcae4ccc4cc864b
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2021-01-16 17:50:10 -0300

    feat: persistencia em arquivo local

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from telegram.ext import (
     Filters,
     ConversationHandler,
     CallbackContext,
+    PicklePersistence
 )
 
 from dotenv import load_dotenv
@@ -134,12 +135,13 @@ def main() -> None:
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
+    pp = PicklePersistence(filename='savedata')
+    updater = Updater(TOKEN,persistence=pp, use_context=True)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    # Add conversation handler with the states
     conv_handler = ConversationHandler(
         entry_points=[
             CommandHandler('start', start),
