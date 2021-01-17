8acee412da1f7093ea36cab9a7d41cc862a7d8b1
4l3xD <alessandra.olea@gmail.com>
2021-01-17 20:35:45 -0300

    add main_association for tests

--- association.py
+++ association.py
@@ -3,7 +3,7 @@ import sys
 import os
 import subprocess as sp
 
-class Chatbot():
+class Association():
     def __init__(self):
         try:
             memory = open('memory.json', 'r')
@@ -25,12 +25,11 @@ class Chatbot():
     def think(self, phrase):
         if phrase in self.phrases:
             return self.phrases[phrase]
-        if phrase == 'Aprende':
-            return 'O que você quer que eu aprenda?'
         if phrase == 'Forms':
             return "https://docs.google.com/forms/d/e/1FAIpQLSdmrdGbOZgiK6GyStj9HTBBXIji4AycF6o2ZDjsmG9udgSP2w/viewform"
-        
         # learning
+        if phrase == 'Aprende':
+            return 'O que você quer que eu aprenda?'    
         lastPhrase = self.historic[-1]
         if lastPhrase == 'O que você quer que eu aprenda?':
             self.key = phrase
@@ -48,10 +47,11 @@ class Chatbot():
         return 'Não entendi...'
 
     def saveMemory(self):
-        memory = open(self.name+'.json', 'w')
-        json.dump([self.known, self.phrases], memory)
+        memory = open('memory.json', 'w')
+        json.dump([self.phrases], memory)
         memory.close()
     
+    # open URLs in the search engine
     def speak(self, phrase):
         if 'Executa ' in phrase:
             platform = sys.platform
@@ -66,4 +66,3 @@ class Chatbot():
         else:
             print(phrase)
         self.historic.append(phrase)
-Chatbot()
---
+++ main_association.py
@@ -0,0 +1,20 @@
+import telepot
+import json
+from chatbot import Chatbot
+
+with open('token.json') as jsonFile:
+    token = json.load(jsonFile)
+telegram = telepot.Bot(token)
+bot = Severina("Severina_Bot")
+
+def receiveMsg(msg):
+    phrase = bot.listen(phrase=msg['text'])
+    response = bot.think(phrase)
+    bot.speak(response)
+    msgType, chatType, chatID = telepot.glance(msg)
+    telegram.sendMessage(chatID, response)
+
+telegram.message_loop(receiveMsg)
+
+while True:
+    pass
